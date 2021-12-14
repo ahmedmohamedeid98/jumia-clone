f7861c6b7bbcf022c809c9909172946b70160ac8
Esraa-Mousad <esraa98mousad@gmail.com>
2021-12-15 00:02:39 +0200

    data part for favorites page done

--- js/favorites.js
+++ js/favorites.js
@@ -4,17 +4,6 @@ $(document).ready(function () {
     const FAVORITES_KEY = "favorites";
     const CARTITEMS_KEY = "cart_items";
 
-    // Product Object
-    var product = function (id, image, title, price, discountedPrice, rating, reviewCount) {
-        this.id = id;
-        this.image = image;
-        this.title = title;
-        this.price = price;
-        this.discountedPrice = discountedPrice;
-        this.rating = rating;
-        this.reviewCount = reviewCount;
-    }
-
     // Cart Item Object
     var cartItem = function (id, image, title, price, discountedPrice, rating, reviewCount, quantity) {
         this.id = id;
@@ -25,24 +14,6 @@ $(document).ready(function () {
         this.rating = rating;
         this.reviewCount = reviewCount;
         this.quantity = quantity;
-    }
-
-    // Note that this list in future will come from localStorage
-    var productsGroup = [
-        new product(1, "../resources/products/1.jpg", "Apple iPhone 13 Single SIM with FaceTime - 256GB - Blue", 1200, 1450, 3, 78),
-        new product(2, "../resources/products/2.jpg", "Apple iPhone 13 Single SIM with FaceTime - 256GB - Blue", 1300, 1450, 3, 78),
-        new product(5, "../resources/products/5.jpg", "Apple iPhone 13 Single SIM with FaceTime - 512GB - Pink", 1750, 1790, 4, 120),
-
-    ];
-
-    // get product item by id
-    function getProduct(id) {
-        var results = productsGroup.filter((prod) => prod.id == id);
-        if (results.length > 0) {
-            return results[0];
-        } else {
-            return {};
-        }
     }
 
     // get cart items list
@@ -60,57 +31,71 @@ $(document).ready(function () {
         localStorage.setItem(CARTITEMS_KEY, JSON.stringify(updatedGroup));
     }
 
+    // get list of products that added to favorites group
+    function getFavorites() {
+        var data = localStorage.getItem(FAVORITES_KEY);
+        if (data) {
+            return JSON.parse(data);
+        } else {
+            return [];
+        }
+    }
+
+    // set new products group to favorites group
+    function restoreUpdatedFavoriteProducts(updatedGroup) {
+        localStorage.setItem(FAVORITES_KEY, JSON.stringify(updatedGroup));
+    }
+
     // // get favorites item
-    // var favoritesGroup = getFavorites();
+    var favoritesGroup = getFavorites();
     // get cart items
     var cartItems = getCartItems();
-    // // get products ids list that belogin to favorites group
-    // var favoritesGroupIds = favoritesGroup.map((prod) => prod.id);
     // get product ids list that belogin to cart items group
     var cartGroupIds = cartItems.map((prod) => prod.id);
 
 
-    for (var i = 0; i < productsGroup.length; i++) {
+    for (var i = 0; i < favoritesGroup.length; i++) {
 
         // current product id
-        var id = productsGroup[i].id;
-        // // check if current product added before to favorites group
-        // var isBelongsToFavorites = favoritesGroupIds.includes(id);
+        var id = favoritesGroup[i].id;
+        
         // check if current product exist in cart items or not
         var isExistInCart = cartGroupIds.includes(id);
 
+        // this list comes from localStorage
         $(".templet-card")
             .clone()
             .last()
             .removeClass("d-none templet-card") // display card and not make it templet
-            .addClass("card-id-" + productsGroup[i].id) // use this class to access card itself
+            .addClass("card-id-" + favoritesGroup[i].id) // use this class to access card itself
             .find('.add_to_cart_btn')
-            .attr("accessKey", productsGroup[i].id)
+            .attr("accessKey", favoritesGroup[i].id)
             .removeClass("disabled") // remove disabled if exist in last cloned element
             .addClass(isExistInCart ? "disabled" : "") // disable add to cart button if this product exist in the cart
             .text(isExistInCart ? "added to cart" : "Add to cart") // disable add to cart button if this product exist in the cart
             .end()
 
             .find('.favorite')
-            .attr("accessKey", productsGroup[i].id)
+            .attr("accessKey", favoritesGroup[i].id)
             .addClass("favorite-on")
             .end()
             .find('img')
-            .attr('src', productsGroup[i].image)
+            .attr('src', favoritesGroup[i].image)
             .end()
             .find('.card-title')
-            .text(productsGroup[i].title)
+            .text(favoritesGroup[i].title)
             .end()
             .find('.price')
-            .text(productsGroup[i].price + "$")
+            .text(favoritesGroup[i].price + "EGP")
             .end()
             .find('.discountedPrice')
-            .text(productsGroup[i].discountedPrice + "$")
+            .text(favoritesGroup[i].discountedPrice + "EGP")
             .end()
             .find('.reviewsCount')
-            .text("(" + productsGroup[i].reviewCount + ")")
+            .text("(" + favoritesGroup[i].reviewCount + ")")
             .end()
             .insertAfter(".product:last");
+            
     }
 
     // Add To Cart Button Listener
@@ -124,11 +109,8 @@ $(document).ready(function () {
 
         console.log("productId: " + productId);
 
-        // TODO : use this product id to add which product object 
+        // use this product id to add which product object 
         // to localStorage at cart products
-
-        // get current product
-        var currentProduct = getProduct(productId);
 
         // create new cart item object
         var cart_item = new cartItem(currentProduct.id,
@@ -143,8 +125,6 @@ $(document).ready(function () {
         cartItems.push(cart_item);
         // resoter cart items group in local storage
         restoreUpdateCartItems(cartItems);
-
-
     });
 
     // Favorite Button Listener 
@@ -153,10 +133,12 @@ $(document).ready(function () {
         // access current product id
         var productId = event.currentTarget.accessKey;
 
+        // check if this product already added to favorite list and we want to remote it 
+        var unfavoriteProduct = event.currentTarget.classList.contains('favorite-on');
+
         // 1. un favorite this product
         event.currentTarget.classList.remove("favorite-on");
         // 2. make this card hidden
-
         // delay hide card 500 millisecond
         $.wait(
             () => {
@@ -165,12 +147,24 @@ $(document).ready(function () {
             200
         );
 
-        // TODO: use this product id to remove this product object 
+        // use this product id to remove this product object 
         // from favorite products in localStorage
 
+        if (unfavoriteProduct) {
+            // update ui
+            event.currentTarget.classList.remove("favorite-on");
+            // remove current product object from favorite products in localStorage
+            for (var i = 0; i < favoritesGroup.length; i++) {
+                if (favoritesGroup[i].id == productId) {
+                    favoritesGroup.splice(i, 1); // remove one item starting from index i
+                    break; // for performance 
+                }
+            }
+            // restore updated favorites group
+            restoreUpdatedFavoriteProducts(favoritesGroup);
+        } 
 
     });
-
 
     $.wait = function (callback, milliseconds) {
         return window.setTimeout(callback, milliseconds);
