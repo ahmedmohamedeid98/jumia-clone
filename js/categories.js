086a500a787611126f4cd06d73ff26448a487423
ahmedmohamedeid98 <ahmedmohamedeid98@gmail.com>
2021-12-15 20:29:12 +0200

    persent product page when click on category card

--- js/categories.js
+++ js/categories.js
@@ -6,21 +6,23 @@ $(document).ready(() => {
         this.title = title;
     }
 
+    
+
 
     var categories = [
         new category(1, "../resources/image/Mobile & Accessories.png", "Mobiles & Accessories"),
-        new category(1, "../resources/image/Mobile & Accessories.png", "Mobiles & Accessories"),
-        new category(1, "../resources/image/Mobile & Accessories.png", "Mobiles & Accessories"),
-        new category(1, "../resources/image/Mobile & Accessories.png", "Mobiles & Accessories"),
-        new category(1, "../resources/image/Mobile & Accessories.png", "Mobiles & Accessories"),
-        new category(1, "../resources/image/Mobile & Accessories.png", "Mobiles & Accessories"),
-        new category(1, "../resources/image/Mobile & Accessories.png", "Mobiles & Accessories"),
-        new category(1, "../resources/image/Mobile & Accessories.png", "Mobiles & Accessories"),
-        new category(1, "../resources/image/Mobile & Accessories.png", "Mobiles & Accessories"),
-        new category(1, "../resources/image/Mobile & Accessories.png", "Mobiles & Accessories"),
-        new category(1, "../resources/image/Mobile & Accessories.png", "Mobiles & Accessories"),
-        new category(1, "../resources/image/Mobile & Accessories.png", "Mobiles & Accessories"),
-        new category(1, "../resources/image/Mobile & Accessories.png", "Mobiles & Accessories"),
+        new category(2, "../resources/index/Men_s Fashion.png", "Men's fashion"),
+        new category(3, "../resources/index/Women_s Fashion.png", "Women's fashion"),
+        new category(4, "../resources/index/TVs & Gaming.png", "TVs & Gaming"),
+        new category(5, "../resources/index/Home & Appliances.png", "Home & Appliances"),
+        new category(6, "../resources/index/Food & Beverage1.png", "Food & Beverage"),
+        new category(7, "../resources/image/Mobile & Accessories.png", "Mobiles & Accessories"),
+        new category(8, "../resources/image/Mobile & Accessories.png", "Mobiles & Accessories"),
+        new category(9, "../resources/image/Mobile & Accessories.png", "Mobiles & Accessories"),
+        new category(10, "../resources/image/Mobile & Accessories.png", "Mobiles & Accessories"),
+        new category(11, "../resources/image/Mobile & Accessories.png", "Mobiles & Accessories"),
+        new category(12, "../resources/image/Mobile & Accessories.png", "Mobiles & Accessories"),
+        new category(13, "../resources/image/Mobile & Accessories.png", "Mobiles & Accessories"),
     ];
 
     for(var i = 0; i < categories.length; i++) {
@@ -37,4 +39,10 @@ $(document).ready(() => {
         .end()
         .insertAfter(".cat-card:last")
     }
+
+    $(".card").on("click", (e) => {
+        console.log(location.pathname);
+        var url = location.origin + location.pathname.replace("categories.html", "products.html");
+    location.replace(url);
+    });
 });
