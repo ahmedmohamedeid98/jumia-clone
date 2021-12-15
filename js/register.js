3381ddadad1a19cb0e7ad0559063236db9be04b8
ahmedmohamedeid98 <ahmedmohamedeid98@gmail.com>
2021-12-15 16:57:12 +0200

    fix bug in register pages

--- js/register.js
+++ js/register.js
@@ -1,163 +1,192 @@
-var x=document.getElementById('fninp');
-var y=document.getElementById('firstnamep');
-var z=document.getElementById('scninp');
-var l=document.getElementById('seconednamep');
-var emailinp=document.getElementById('Emailsinp');
-var emaillap=document.getElementById('emaillabel');
-var passwordinp=document.getElementById('passes');
-var passlap=document.getElementById('passwordlabel');
-var phoneNumber =document.getElementById('phonenumber');
+var fnameInput = document.getElementById("fname");
+var lnameInput = document.getElementById("lname");
+
+var emailInput = document.getElementById("email");
+var emailInvalidSpan = document.getElementById("invalid-email");
+
+var passwordInput = document.getElementById("password");
+var passwordInvalidSpan = document.getElementById("invalid-password");
+
+var phoneInput = document.getElementById("phone");
+var phoneInvalidSpan = document.getElementById("invalid-phone");
+
+var gender = document.getElementsByName("gender");
+var genderInvalidSpan = document.getElementById("invalid-gender");
+var genderStr = "male"; // init value
+
+var emailExistAlert = document.getElementById("existance-alert");
+
+var loginBtn = document.getElementById("login-btn");
+var resiterBtn = document.getElementById("register-btn");
+
+loginBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    re_route_to_login();
+});
 
 
 
-x.addEventListener('click',function(){
-    y.innerHTML="first name";
-    y.style.color='orange';
-    x.style.outlineColor='white';
-    x.style.borderBottomColor='orange'
-    
-   
-})
-
-z.addEventListener('click',function(){
-l.innerHTML="last name";
-l.style.color='orange';
-z.style.outlineColor='white';
-})
-
-emailinp.addEventListener('click',function(){
-    emaillap.innerHTML="Email";
-    emailinp.style.borderBottomColor='orange';
-    emaillap.style.color='orange';
-    emailinp.style.outlineColor='white';
-
-})
-passwordinp.addEventListener('click',function(){
-    passlap.innerHTML="password";
-    passlap.style.color='orange'
-    passwordinp.style.borderBottomColor='orange';
-    passwordinp.style.outlineColor='white';
-})
-
-
-
-// function validatePhoneNumber(inputtxt) {
-//     var pattern = /^[0][1][0 1 2 5]\d{9}$/;
-//     if (inputtxt.match(pattern)) {
-//         return true;
-//     } else {
-//         return false;
-//     }
-// }
-
-// function validateEmail(inputtxt) {
-//     var emailPattern = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
-//     if (inputtxt.match(emailPattern)) {
-//         return true;
-//     } else {
-//         return false;
-//     }
-// }
-
-function validation(){
-    var emailPattern = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
- if(emailPattern.test(emailinp)){
-document.getElementById('useerror').innerHTML="";
- } else{
-    document.getElementById('useerror').innerHTML="Email is invailed ";
-    return false;  
- }
-
-
-
-    var pattern = /^[0][1][0 1 2 5]\d{9}$/;
-     if (pattern.test(phoneNumber)) {
-        document.getElementById('useerror').innerHTML="";
-    } else {
-        document.getElementById('useerror').innerHTML="password is invailed ";
-       return false;
-     }
+function re_route_to_login() {
+    var url = location.origin + location.pathname.replace("register.html", "login.html");
+    location.replace(url);
 }
 
+//=============================
+//       Validation Methods
+//=============================
+
+function validatePhoneNumber() {
+    var pattern = /^[0][1][0 1 2 5]\d{8}$/;
+    console.log(phoneInput.value);
+    if (phoneInput.value != null && phoneInput.value.match(pattern)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validateEmail() {
+    var emailPattern = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+    console.log(emailInput.value);
+    if (emailInput.value != null && emailInput.value.match(emailPattern)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validatePassword() {
+    if (passwordInput.value != null && passwordInput.value.length >= 8) {
+        return true;
+    }
+    return false;
+}
+
+function validateGender() {
+    for (var i in gender) {
+        if (gender[i].checked) {
+            genderStr = i == 0 ? "male" : "female";
+            return true;
+        }
+    }
+    return false;
+}
+
+//==============================
+//       Submit method
+//==============================
+
+function resetAllErrorsToBeHidden() {
+    if (!emailInvalidSpan.classList.contains("d-none"))
+        emailInvalidSpan.classList.add("d-none");
+
+    if (!passwordInvalidSpan.classList.contains("d-none"))
+        passwordInvalidSpan.classList.add("d-none");
+
+    if (!phoneInvalidSpan.classList.contains("d-none"))
+        phoneInvalidSpan.classList.add("d-none");
+
+    if (!genderInvalidSpan.classList.contains("d-none"))
+        genderInvalidSpan.classList.add("d-none");
+
+    if (!emailExistAlert.classList.contains("d-none"))
+        emailExistAlert.classList.add("d-none");
+
+}
+
+resiterBtn.addEventListener("click", (e) => {
+
+    resetAllErrorsToBeHidden();
+    console.log("sumbit");
+    var everyThingValid = true;
+    if (!validateEmail()) {
+        console.log("email is not valid");
+        if (emailInput.value != "")
+            emailInvalidSpan.classList.remove("d-none");
+        everyThingValid = false;
+    }
+    if (!validatePassword()) {
+        if (passwordInput.value != "")
+            passwordInvalidSpan.classList.remove("d-none");
+        everyThingValid = false;
+    }
+    if (!validatePhoneNumber()) {
+        if (phoneInput.value != "")
+            phoneInvalidSpan.classList.remove("d-none");
+        everyThingValid = false;
+    }
+    if (!validateGender()) {
+        genderInvalidSpan.classList.remove("d-none");
+        everyThingValid = false;
+    }
 
 
 
+    if (everyThingValid) {
+        e.preventDefault();
+
+        if (isUserExistInDB(emailInput.value)) {
+            document.getElementById("existance-alert").classList.remove("d-none");
+        } else {
+
+            var users = getUsersList();
+
+            var newUser = new user(
+                users.length + 1,
+                fnameInput.value,
+                lnameInput.value,
+                emailInput.value,
+                phoneInput.value,
+                passwordInput.value,
+                genderStr);
+
+            users.push(newUser);
+            restoreUserList(users);
+            re_route_to_login();
+
+        }
+    } else {
+        e.preventDefault();
+
+    }
+});
 
 
+//=======================
+// Database Section
+//=======================
 
+var user = function (id, fname, lname, email, phone, password, gender) {
+    this.id = id;
+    this.fname = fname;
+    this.lname = lname;
+    this.email = email;
+    this.password = password;
+    this.phone = phone;
+    this.gender = gender;
+}
 
+const USERS_KEY = "users";
 
+function getUsersList() {
+    var data = localStorage.getItem(USERS_KEY);
+    if (data) {
+        return JSON.parse(data); // [user]
+    } else {
+        return [];
+    }
+}
 
+function restoreUserList(newUsersList) {
+    localStorage.setItem(USERS_KEY, JSON.stringify(newUsersList));
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// $(document).ready(() => {
-
-//     const USERS_KEY = "users";
-
-//     var user = function(id, fname, lname, email, phone, password, gender) {
-//         this.id = id;
-//         this.fname = fname;
-//     }
-
-//     function getUsersList() {
-//         var data = localStorage.getItem(USERS_KEY);
-//         if(data) {
-//             return JSON.parse(data); // [user]
-//         } else {
-//             return [];
-//         }
-//     }
-
-//     function restoreUserList(newUsersList) {
-//         localStorage.setItem(USERS_KEY, JSON.stringify(newUsersList));
-//     }
-
-//     function isUserExistInDB(email) {
-//         var userList = getUsersList();
-//         for(var i = 0; i < userList.length; i++) {
-//             if(userList[i].email == email) {
-//                 return true;
-//             }
-//         }
-//         return false;
-//     }
-    
-//     if(isUserExistInDB("d;flksd")) {
-//         // show alert
-//     } else {
-//         var usersList = getUsersList();
-//         // create new user object
-//         var newUser = new user(dslfksdfgl);
-//         // push new user to getted userList from database
-//         usersList.push(newUser);
-//         // restore in database
-//         restoreUserList(usersList);
-//         // re-route to login page
-//         var url = location.origin + location.pathname.replace("register.html", "login.html");
-//         location.replace(url);
-//     }
-
-
-
-// });
+function isUserExistInDB(email) {
+    var userList = getUsersList();
+    for (var i = 0; i < userList.length; i++) {
+        if (userList[i].email == email) {
+            return true;
+        }
+    }
+    return false;
+}
