a40290837abb57946101b6c4e2fe59fcf7defe51
Esraa-Mousad <esraa98mousad@gmail.com>
2021-12-18 01:59:00 +0200

    sign in out

--- js/navbar.js
+++ js/navbar.js
@@ -3,14 +3,14 @@ $(document).ready(() => {
 
     var isUserSignedIn = localStorage.getItem("is_logged_in");
 
-    if (isUserSignedIn) {
+    if (JSON.parse(isUserSignedIn) === true) {
         $("#sign_in_out").text("Sign Out");
     } else {
         $("#sign_in_out").text("Sign In");
     }
 
     $("#sign_in_out").on("click", () => {
-        if (isUserSignedIn) {
+        if (JSON.parse(isUserSignedIn) === true) {
             localStorage.setItem("is_logged_in", false);
         }
     });
