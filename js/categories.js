f536dc344809ce188c544902698203a98d5cf4f0
Esraa-Mousad <esraa98mousad@gmail.com>
2021-12-17 12:30:33 +0200

    cat 2

--- js/categories.js
+++ js/categories.js
@@ -10,30 +10,30 @@ $(document).ready(() => {
 
 
     var categories = [
-        new category(1, "../resources/image/Kids & Baby.png", "Kids & Baby"),
-        new category(2, "../resources/index/Men_s Fashion.png", "Men's fashion"),
-        new category(3, "../resources/index/Beauty & perfumes.png", "Beauty & perfumes"),
-        new category(4, "../resources/index/TVs & Gaming.png", "TVs & Gaming"),
-        new category(5, "../resources/index/Home & Appliances.png", "Home & Appliances"),
-        new category(6, "../resources/index/Food & Beverage1.png", "Food & Beverage"),
-        new category(7, "../resources/image/Laptop & Accessories.png", "Laptop & Accessories"),
-        new category(8, "../resources/image/Car Essentials.png", "Car Essentials"),
-        new category(9, "../resources/image/Household Care.png", "Household Care"),
-        new category(10, "../resources/image/Mobile & Accessories.png", "Mobiles & Accessories"),
-        new category(11, "../resources/image/Sporting.png", "Sporting"),
-        new category(12, "../resources/image/Women_s Fashion.png", "Women_s Fashion"),
+        new category(10, "../resources/categories/Mobile & Accessories.png", "Mobiles & Accessories"),
+        new category(1, "../resources/categories/Kids & Baby.png", "Kids & Baby"),
+        new category(2, "../resources/categories/Men_s Fashion.png", "Men's fashion"),
+        new category(3, "../resources/categories/Beauty & perfumes.png", "Beauty & perfumes"),
+        new category(4, "../resources/categories/TVs & Gaming.png", "TVs & Gaming"),
+        new category(5, "../resources/categories/Home & Appliances.png", "Home & Appliances"),
+        new category(6, "../resources/categories/Food & Beverage.png", "Food & Beverage"),
+        new category(7, "../resources/categories/Laptop & Accessories.png", "Laptop & Accessories"),
+        new category(8, "../resources/categories/Car Essentials.png", "Car Essentials"),
+        new category(9, "../resources/categories/Household Care.png", "Household Care"),
+        new category(11, "../resources/categories/Sporting.png", "Sporting"),
+        new category(12, "../resources/categories/Women_s Fashion.png", "Women's Fashion"),
     ];
 
     for(var i = 0; i < categories.length; i++) {
         var currectCategory = categories[i];
-        $(".templet-cart")
+        $(".templet-card")
         .clone()
         .last()
-        .removeClass("d-none templet-cart")
+        .removeClass("d-none templet-card")
         .find(".cat-img")
         .attr("src", currectCategory.image)
         .end()
-        .find(".cart-title")
+        .find(".card-title")
         .text(currectCategory.title)
         .end()
         .insertAfter(".cat-card:last")
